5cdfed515d041ffd30402935f7e5d1a2053c8397
eliftt1 <eliftuncay83@gmail.com>
2025-11-16 20:39:45 +0300

    Update test_game.py

--- test_game.py
+++ test_game.py
@@ -1,18 +1,20 @@
 import unittest
 import os
 import json
-import game 
+import game  
 
 class TestCalcAndHang(unittest.TestCase):
 
-    # Testlerin her birinden önce çalışan hazırlık fonksiyonu
+    # Testlerin her birinden önce çalışır
     def setUp(self):
         self.test_scores_file = "test_scores.json"
         game.SCORES_FILE = self.test_scores_file
         
+
         if os.path.exists(self.test_scores_file):
             os.remove(self.test_scores_file)
 
+    
     def tearDown(self):
         if os.path.exists(self.test_scores_file):
             os.remove(self.test_scores_file)
@@ -27,11 +29,14 @@ class TestCalcAndHang(unittest.TestCase):
     def test_rastegele_kelime_secimi(self):
         words_dict = game.kategori_verisi()
         kategori, kelime = game.rastegele_kelime_secimi(words_dict)
+        
         self.assertIn(kategori, words_dict)
         self.assertIn(kelime, words_dict[kategori])
         self.assertEqual(kelime, kelime.lower()) 
 
     def test_rastgele_harf_sec(self):
+
+        #Tek bir harfi açma
         kelime = "elma"
         masked = ["_", "l", "m", "_"]
         opened = game.rastgele_harf_sec(kelime, masked)
@@ -39,7 +44,7 @@ class TestCalcAndHang(unittest.TestCase):
         self.assertIn(opened, ['e', 'a'])
         if opened == 'e':
             self.assertEqual(masked, ["e", "l", "m", "_"])
-        else:
+        else: 
             self.assertEqual(masked, ["_", "l", "m", "a"])
 
         # Birden fazla aynı harfi açma
@@ -50,33 +55,35 @@ class TestCalcAndHang(unittest.TestCase):
         self.assertEqual(opened, 'a')
         self.assertEqual(masked, ["a", "n", "a", "n", "a", "s"])
 
-        # Tüm harfler zaten açıksa 
+        # Tüm harfler zaten açıksa
         kelime = "kedi"
         masked = ["k", "e", "d", "i"]
         opened = game.rastgele_harf_sec(kelime, masked)
-        self.assertIsNone(opened)
+        self.assertIsNone(opened) 
 
+   
     def test_skor_kaydet(self):
-        game.skor_kaydet("Ali", 100)
-        game.skor_kaydet("Veli", 300)
-        game.skor_kaydet("Ayşe", 50)    
-        game.skor_kaydet("Fatma", 200)
-        game.skor_kaydet("Zeki", 400)
-        game.skor_kaydet("Mert", 250)   
+        game.skor_kaydet("reyhan", 100)
+        game.skor_kaydet("nisa", 300)
+        game.skor_kaydet("Ayşe", 50)     
+        game.skor_kaydet("zehra", 200)
+        game.skor_kaydet("elif", 400)
+        game.skor_kaydet("Mert", 250)     
 
         self.assertTrue(os.path.exists(self.test_scores_file))
 
+        # Dosyayı okur ve doğrular
         with open(self.test_scores_file, "r", encoding="utf-8") as f:
             scores = json.load(f)
-            
+        
         self.assertEqual(len(scores), 5)
         
-        # Skorlar doğru sıralanmış mı? 
-        self.assertEqual(scores[0]["name"], "Zeki")  
-        self.assertEqual(scores[1]["name"], "Veli")  
-        self.assertEqual(scores[2]["name"], "Mert") 
-        self.assertEqual(scores[3]["name"], "Fatma") 
-        self.assertEqual(scores[4]["name"], "Ali")  
+        self.assertEqual(scores[0]["name"], "elif")  
+        self.assertEqual(scores[1]["name"], "nisa")  
+        self.assertEqual(scores[2]["name"], "Mert")  
+        self.assertEqual(scores[3]["name"], "zehra") 
+        self.assertEqual(scores[4]["name"], "reyhan")  
+
 
 if __name__ == '__main__':
     unittest.main()
